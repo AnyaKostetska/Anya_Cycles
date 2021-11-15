c58524e7d6b7fde801c2e423692831bce3553703
annkostet <annakostetska30@gmail.com>
2021-11-15 21:37:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Anya_Cycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
